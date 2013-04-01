89683f91230fc58d9d9168801c73dc2c0678fe43
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-04-01 21:46:01 +0200

    Add time aggregations

--- src/com/ifesdjeen/utils/seq_utils.clj
+++ src/com/ifesdjeen/utils/seq_utils.clj
@@ -5,12 +5,6 @@
   [vec value]
   (some #(= value %) vec))
 
-
 (defmacro multi-update
   [m & update-forms]
   `(-> ~m ~@(map #(cons 'update-in %) update-forms)))
-
-(commeny (multi-update {:a 10 :b 20 :c 30}
-                       ([:a] + 1)
-                       ([:b] + 1 1)
-                       ([:c] + 1 2)))
