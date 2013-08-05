b05d60f033ed1c4e82a4e6250e5ae95dafebec83
Alex Petrov <alexp@coffeenco.de>
2013-08-05 22:36:36 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ifesdjeen/utils "0.4.0"
+(defproject com.ifesdjeen/utils "0.5.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
